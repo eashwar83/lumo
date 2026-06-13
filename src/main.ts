25dc1e0596703c19f2f0ce77662da3e40884086f
FengZeng <zengfeng@pm.me>
2026-06-13 12:25:41 +0800

    fix(ui): resolve window transparency issues on linux and windows

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,14 @@ import { createApp } from "vue";
 import App from "./App.vue";
 // import "./styles.css";
 
+const userAgent = window.navigator.userAgent;
+
+if (/\bwindows\b/i.test(userAgent)) {
+    document.documentElement.setAttribute("data-platform", "windows");
+} else if (/\blinux\b/i.test(userAgent)) {
+    document.documentElement.setAttribute("data-platform", "linux");
+}
+
 if (!import.meta.env.DEV) {
     window.addEventListener("contextmenu", (event) => {
         event.preventDefault();
